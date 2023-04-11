4fe00bef20ff737610519755b35b036832ed290c
Marius Petria <mpetria@adobe.com>
2023-04-11 21:18:35 +0300

    move sidenav after main section for mobile
    adaptive layout based on media type
    
    temp floats
    
    fix mobile floats
    
    fix header height
    
    fix linter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,11 +32,11 @@ function buildHeroBlock(main) {
 }
 
 function buildSidebar(main) {
-  main.classList.add('main-content');
+  main.querySelectorAll(':scope > div').forEach((section) => section.classList.add('main-content'));
   const sidenav = document.createElement('div');
-  sidenav.classList.add('sidenav');
+  sidenav.classList.add('side-content');
   sidenav.append(buildBlock('sidenav', ' '));
-  main.prepend(sidenav);
+  main.append(sidenav);
 }
 
 /**
