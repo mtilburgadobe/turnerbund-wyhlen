6e4d069ce56bd1423bb169540dc866082148e583
Mike Tilburg <mtilburg@adobe.com>
2023-04-12 16:12:45 +0200

    initial footer implementation, missing functionality, will open new issues

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -21,5 +21,38 @@ export default async function decorate(block) {
 
     decorateIcons(footer);
     block.append(footer);
+
+    const blockChildren = footer.firstElementChild.children;
+    footer.firstElementChild.classList.add('footer-title');
+
+    const footerGallery = document.createElement('div');
+    footerGallery.classList.add('footer-gallery');
+    footer.append(footerGallery);
+
+    const footerImpressum = document.createElement('div');
+    footerImpressum.classList.add('footer-impressum');
+    footer.append(footerImpressum);
+
+    const numChildren = blockChildren.length;
+    for (let i = 0; i < numChildren; i += 1) {
+      const thisBlock = blockChildren[i];
+      if (thisBlock && thisBlock.tagName === 'P') {
+        if (thisBlock.children[0]
+            && thisBlock.children[0].tagName === 'A'
+            && thisBlock.children[0].children[0]
+            && thisBlock.children[0].children[0].tagName === 'PICTURE') {
+          thisBlock.classList.add('footer-gallery-item');
+          footerGallery.appendChild(thisBlock);
+          i -= 1;
+        } else {
+          thisBlock.classList.add('footer-impressum');
+          footerImpressum.appendChild(thisBlock);
+          i -= 1;
+        }
+      } else if (thisBlock && thisBlock.tagName === 'UL') {
+        thisBlock.classList.add('impressum');
+        footerImpressum.appendChild(thisBlock);
+      }
+    }
   }
 }
