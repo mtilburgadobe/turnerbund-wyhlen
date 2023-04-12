b970d49b7c0bbd8466fc952a47055dffdd0c3148
Mike Tilburg <mtilburg@adobe.com>
2023-04-12 14:19:13 +0200

    implment initial footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -21,5 +21,39 @@ export default async function decorate(block) {
 
     decorateIcons(footer);
     block.append(footer);
+
+    const blockChildren = footer.firstElementChild.children;
+    footer.firstElementChild.classList.add('footer-title');
+    const footerGallery = document.createElement('div');
+    footerGallery.classList.add('footer-gallery');
+    const footerImpressum = document.createElement('div');
+    footerImpressum.classList.add('footer-impressum');
+    footer.append(footerGallery);
+    footer.append(footerImpressum);
+
+    const numChildren = blockChildren.length;
+    for (let i = 0; i < numChildren; i += 1) {
+      console.log(blockChildren[i]);
+      //console.log(numChildren);
+      
+      if (blockChildren[i] && blockChildren[i].tagName == 'P'){
+        if (blockChildren[i].children[0] &&
+            blockChildren[i].children[0].tagName == 'A' &&
+            blockChildren[i].children[0].children[0] &&
+            blockChildren[i].children[0].children[0].tagName == 'PICTURE') {
+          blockChildren[i].classList.add("footer-gallery-item")
+          footerGallery.appendChild(blockChildren[i]);
+        } else {
+          blockChildren[i].classList.add("footer-impressum")
+          footerImpressum.appendChild(blockChildren[i]);
+        }
+        i -= 1;
+      } else if (blockChildren[i] && blockChildren[i].tagName == 'UL') {
+        blockChildren[i].classList.add("impressum")
+        footerImpressum.appendChild(blockChildren[i]);
+        i -= 1;
+      }
+    }
+
   }
 }
