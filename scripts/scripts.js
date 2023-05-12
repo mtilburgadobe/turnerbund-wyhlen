f583d265e8fb59abe6853dad9b7f6bb9eca6f878
Mike Tilburg <mtilburg@adobe.com>
2023-05-12 08:09:03 +0200

    try blog sidekick code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -160,23 +160,9 @@ async function loadPage() {
   loadDelayed();
 }
 
-const preflight = ({ detail }) => {
-  const sk = detail.data;
-  // your custom code from button.action goes here
-  // eslint-disable-next-line no-console
-  console.log(sk);
-};
-
-const sk = document.querySelector('helix-sidekick');
-if (sk) {
-  // sidekick already loaded
-  sk.addEventListener('custom:preflight', preflight);
-} else {
-  // wait for sidekick to be loaded
-  document.addEventListener('helix-sidekick-ready', () => {
-    document.querySelector('helix-sidekick')
-      .addEventListener('custom:preflight', preflight);
-  }, { once: true });
-}
-
 loadPage();
+
+// load plugins when sidekick is ready
+document.addEventListener('helix-sidekick-ready', () => {
+  import('../tools/sidekick/plugins.js');
+}, { once: true });
